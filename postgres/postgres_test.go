3e109df0d58140fb3caec270a21c033ba93eabde
Slava <development1810@gmail.com>
2019-11-05 16:04:22 +0200

    update deleting and testing

--- postgres/postgres_test.go
+++ postgres/postgres_test.go
@@ -8,7 +8,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/ory/dockertest"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func CreatePostgresForTesting(t testing.TB) (*sql.DB, func()) {
@@ -17,7 +17,7 @@ func CreatePostgresForTesting(t testing.TB) (*sql.DB, func()) {
 		t.Fatal(err)
 	}
 
-	cont, err := pool.Run("postgres", "latest", nil)
+	cont, err := pool.Run("postgres", "latest", []string{"POSTGRES_PASSWORD=master", "POSTGRES_DB=musicDB"})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,10 +31,9 @@ func CreatePostgresForTesting(t testing.TB) (*sql.DB, func()) {
 
 	err = pool.Retry(func() error {
 
-		connStr := "user=sorter password=master host=localhost port=5432 dbname=musicDB sslmode=disable"
-		fmt.Printf("connStr = '%v'\n", connStr)
+		//connStr := "user=sorter password=master host=localhost port=5432 dbname=musicDB sslmode=disable"
 
-		cli, err := sql.Open("postgres", connStr)
+		cli, err := sql.Open("postgres", fmt.Sprintf("postgres://postgres:master@%s/%s?sslmode=disable", addr, "musicDB"))
 		if err != nil {
 			return err
 		}
@@ -46,7 +45,7 @@ func CreatePostgresForTesting(t testing.TB) (*sql.DB, func()) {
 		t.Fatal(err)
 	}
 
-	connPostgres, err := sql.Open("postgres", addr)
+	connPostgres, err := sql.Open("postgres", fmt.Sprintf("postgres://postgres:master@%s/%s?sslmode=disable", addr, "musicDB"))
 	if err != nil {
 		cont.Close()
 		t.Fatal(err)
@@ -74,10 +73,42 @@ func TestDatabase_AddAlbum(t *testing.T) {
 
 	errCreate := testDB.CreateTable(CreateTableALBUM)
 	fmt.Printf("errCreate = '%v'\n", errCreate)
-	assert.Nil(t, errCreate)
+	fmt.Printf("errCreate.Error() ='%v'\n", errCreate.Error())
 
+	require.Nil(t, errCreate)
 	//gotID, gotErr := testDB.AddAlbum()
 
 }
 
 // https://postgresql.leopard.in.ua/
+
+func Example() {
+	var (
+		db  *sql.DB
+		err error
+	)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.Run("postgres", "9.6", []string{"POSTGRES_PASSWORD=master", "POSTGRES_DB=musicDB"})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err = pool.Retry(func() error {
+		database := "musicDB"
+		var err error
+		db, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", resource.GetPort("5432/tcp"), database))
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// When you're done, kill and remove the container
+	err = pool.Purge(resource)
+}
